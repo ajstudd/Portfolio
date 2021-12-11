f5e6248fc7103119df1c5ab23e82d70ac660a0c1
ajstudd <j7654894110@gmail.com>
2021-12-11 15:02:14 +0530

    edited error page

--- pages/_error.js
+++ pages/_error.js
@@ -1,4 +1,12 @@
-Error.getInitialProps = ({ res, err }) => {
+import Error from "next/error";
+
+function Page({ statusCode }: any) {
+  return <Error statusCode={statusCode}></Error>;
+}
+
+Page.getInitialProps = ({ res, err }: { res: any, err: any }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
   return { statusCode };
 };
+
+export default Page;
