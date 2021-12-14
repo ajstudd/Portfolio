5e226ee2b054b6be7b441233e68675fcdbfd919d
ajstudd <j7654894110@gmail.com>
2021-12-14 18:26:05 +0530

    initialproposition

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+import { Heading, HStack, Flex, Spacer, Box } from "@chakra-ui/react";
+import React from "react";
+import {} from "react-icons/ai";
+import {} from "react-icons/md";
+
+function Header() {
+  return (
+    <div>
+      <HStack alignItems="center" justifyContent="center" bg="gray.100">
+        <Heading>AJX</Heading>
+      </HStack>
+    </div>
+  );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,15 @@
-import {
-  Box,
-  Center,
-  Heading,
-  HStack,
-  VStack,
-  SimpleGrid,
-  Spacer,
-  flexbox,
-  Flex,
-} from "@chakra-ui/react";
-import {
-  AiOutlineInstagram,
-  AiOutlineTwitter,
-  AiOutlineMail,
-} from "react-icons/ai";
+import { Box, Text, Heading, VStack, Flex } from "@chakra-ui/react";
+import Header from "../components/Header";
+import {} from "react-icons/ai";
 export default function Home() {
   return (
     <div>
+      <Header />
       <Flex justifyContent="center" alignItems="center" height="100vh">
-        <Heading>Aapka Swagat hai</Heading>
+        <VStack>
+          <Heading>Aapka Swagat hai</Heading>
+          <Text>Website pe Kaam chalu hai</Text>
+        </VStack>
       </Flex>
     </div>
   );
