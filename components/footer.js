fe00f1d90ce0e1acbf350bc67d6af573fc53e754
ajstudd <j7654894110@gmail.com>
2021-12-18 23:27:20 +0530

    edited footer and icons

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,72 @@
-import { Box, Heading, HStack, Spacer } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  HStack,
+  Spacer,
+  Divider,
+  Center,
+} from "@chakra-ui/react";
+import { FiSend, FiInstagram, FiMail } from "react-icons/fi";
 import React from "react";
 
 function footer() {
   return (
     <div>
-      <HStack py="5" px="10" bg="gray.800">
-        <Box>yo</Box>
+      <HStack spacing="5" py="5" px="10" bg="gray.800">
+        <Box
+          onClick={() => {
+            window.open("https://t.me/ajstudd31");
+          }}
+          textColor="white"
+          cursor="pointer"
+          _hover={{
+            textColor: `gray.600`,
+          }}
+        >
+          <FiSend />
+        </Box>
+        <Box
+          onClick={() => {
+            window.open("https://www.instagram.com/sav__aj/");
+          }}
+          textColor="white"
+          cursor="pointer"
+          _hover={{
+            textColor: `gray.600`,
+          }}
+        >
+          <FiInstagram />
+        </Box>
+        <Box
+          onClick={() => {
+            window.open("mailto:j7654894110@gmail.com");
+          }}
+          textColor="white"
+          cursor="pointer"
+          _hover={{
+            textColor: `gray.600`,
+          }}
+        >
+          <FiMail />
+        </Box>
         <Spacer />
-        <Box>aj</Box>
+        <Box
+          textColor="white"
+          cursor="pointer"
+          _hover={{
+            textColor: `gray.600`,
+          }}
+        >
+          Feedback
+        </Box>
+      </HStack>
+      <Center>
+        <Box height="1px" width="full" bg="gray.700"></Box>
+      </Center>
+      <HStack py="-1" bg="gray.800" alignItems="center" justifyContent="center">
+        <Box textColor="white" fontWeight="light">
+          Designed by AJ
+        </Box>
       </HStack>
     </div>
   );
