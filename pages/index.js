399f33ccbc876984887ec9d73ed48421095e7895
ajstudd <j7654894110@gmail.com>
2021-12-21 22:00:10 +0530

    added quotes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-import { Box, Text, Heading, VStack, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Heading,
+  VStack,
+  Flex,
+  Button,
+  AspectRatio,
+  Image,
+} from "@chakra-ui/react";
+import Modalpop from "../components/modalpop";
 import Header from "../components/Header";
 import Footer from "../components/footer";
-import { MdConstruction } from "react-icons/md";
+import { MdConstruction, MdArrowDownward } from "react-icons/md";
+
 export default function Home() {
   return (
     <div>
@@ -12,34 +23,48 @@ export default function Home() {
         height="81vh"
         bgGradient="linear(to-r, #D4F3EF, #30475E)"
       >
-        <VStack border="2px" w="xl" rounded="10">
-          <Heading
-            mt="3.5"
-            userSelect="none"
-            _hover={{
-              textColor: `gray.300`,
-            }}
-          >
-            Aapka Swagat hai
-          </Heading>
-          <Text
-            userSelect="none"
-            _hover={{
-              textColor: `gray.300`,
-            }}
-          >
-            Website pe Kaam chalu hai
-          </Text>
-          <Box
-            _hover={{
-              textColor: `gray.300`,
-            }}
-          >
-            <MdConstruction fontSize="25" />
-          </Box>
-          <Box mb="3.5" />
+        <VStack>
+          <VStack border="2px" w="xs" rounded="10">
+            <Heading
+              mt="3.5"
+              userSelect="none"
+              _hover={{
+                textColor: `gray.300`,
+              }}
+            >
+              Aapka Swagat hai
+            </Heading>
+            <Text
+              userSelect="none"
+              _hover={{
+                textColor: `gray.300`,
+              }}
+            >
+              Get some life tips here
+            </Text>
+            <Box
+              _hover={{
+                textColor: `gray.300`,
+              }}
+            >
+              <MdArrowDownward fontSize="25" />
+            </Box>
+            <Box mb="3.5" />
+          </VStack>
+
+          <Modalpop />
         </VStack>
       </Flex>
+      <Text
+        bgGradient="linear(to-r, #D4F3EF, #30475E)"
+        fontWeight="hairline"
+        userSelect="none"
+        _hover={{
+          textColor: `green`,
+        }}
+      >
+        !This Website is still under construction!
+      </Text>
       <Footer />
     </div>
   );
