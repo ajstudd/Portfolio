a12df8f0f500b1e24c908de667ce964596c02af2
ajstudd <j7654894110@gmail.com>
2022-11-09 14:47:44 +0530

    added meta tags

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ import {
   SimpleGrid,
 } from "@chakra-ui/react";
 import head from "next/head";
+import { useRouter } from "next/router";
 import Header from "../components/Header";
 import Card from "../components/card";
 import Banner from "../components/banner";
@@ -17,12 +18,47 @@ import { useState } from "react";
 import About from "../components/about";
 import Footer from "../components/footer";
 export default function Home() {
+  const router = useRouter();
+  const canonicalUrl = (
+    `https://www.junaid.works` + (router.asPath === "/" ? "" : router.asPath)
+  ).split("?")[0];
   const [contact, setContact] = useState(false);
   return (
     <>
       {/* Imported head from next and created a title to show in browser */}
       <head>
         <title>Junaid Ahmad | Frontend</title>
+        <link rel="canonical" href={canonicalUrl} />
+
+        <meta
+          name="description"
+          content="Find information about me and my work here."
+        />
+
+        <meta property="og:url" content="https://www.junaid.works/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Junaid Ahmad | Frontend" />
+        <meta
+          property="og:description"
+          content="Find information about me and my work here."
+        />
+        <meta
+          property="og:image"
+          content="https://ajstudd.github.io/Image/WhatsApp%20Image%202021-01-16%20at%2010.42.45%20AM.jpeg"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="junaid.works" />
+        <meta property="twitter:url" content="https://www.junaid.works/" />
+        <meta name="twitter:title" content="Junaid Ahmad | Frontend" />
+        <meta
+          name="twitter:description"
+          content="Find information about me and my work here."
+        />
+        <meta
+          name="twitter:image"
+          content="https://ajstudd.github.io/Image/WhatsApp%20Image%202021-01-16%20at%2010.42.45%20AM.jpeg"
+        />
       </head>
       <Box bgGradient="linear(to-r, #D4F3EF, #30475E)" w="full">
         <Header />
